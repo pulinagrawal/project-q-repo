a510db355d31e25e3fcfbfa7117c19bbc2a14e59
Soujanya27 <soujanya.chatterjee@gmail.com>
2015-04-07 01:25:27 +0000

    Category Landing Page updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,11 @@
 
 
   Profile.create(first_name: 'TestFirstName', last_name: 'testlast',birthday: '1/1/2014', password: 'test',  email: 'test@test.com')
+  Category.create(category_name: 'Mathematics')
+  Category.create(category_name: 'History')
+  Category.create(category_name: 'Geography')
+  Category.create(category_name: 'Sports')
+  Category.create(category_name: 'Movies')
+  Category.create(category_name: 'Fun')
+  Category.create(category_name: 'Cartoon')
+  Category.create(category_name: 'Science')

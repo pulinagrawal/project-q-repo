b291fdc08fa06826f96e9b41027b5322ba2d4738
Craig Kelly <craig.n.kelly@gmail.com>
2015-04-08 19:10:47 +0000

    quiz question stepping

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,13 +1,40 @@
 class QuizzesController < ApplicationController
     def deduce_quiz_state
-        #todo: reference @quiz
+        @quiz_done = false
         @current_question_num = 1
-        @current_question = "Need question impl?"
+        @current_question = nil
+        
+        if not @quiz.question5.nil?
+            @quiz_done = true
+            @current_question_num = nil
+            question_index = nil
+        elsif not @quiz.question4.nil?
+            @current_question_num = 5
+            question_index = @quiz.question5
+        elsif not @quiz.question3.nil?
+            @current_question_num = 4
+            question_index = @quiz.question4
+        elsif not @quiz.question2.nil?
+            @current_question_num = 3
+            question_index = @quiz.question3
+        elsif not @quiz.question1.nil?
+            @current_question_num = 2
+            question_index = @quiz.question2
+        else
+            @current_question_num = 1
+            question_index = @quiz.question1
+        end
+        
+        if not question_index.nil?
+            @current_question = Question.find(question_index)
+        end
     end
 
     def show
         @quiz = Quiz.find(params[:id])
         deduce_quiz_state
+        
+        #TODO: what if quiz done?
     end
     
     def answer

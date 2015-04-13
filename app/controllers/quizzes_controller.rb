3c584842dd7c9f8f27ab3a889317798c3c3ed5be
Craig Kelly <craig.n.kelly@gmail.com>
2015-04-12 23:19:22 -0500

    quiz q&a with progression

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -4,32 +4,34 @@ class QuizzesController < ApplicationController
         @quiz_done = false
         @current_question_num = 1
         @current_question = nil
-        
+
         question_index = nil
-        
-        if not @quiz.correct5.blank?
+
+        logger.debug @quiz.as_json
+
+        if not @quiz.correct5.nil?
             #All questions answered
             @quiz_done = true
             @current_question_num = nil
         else
             #Find where they are at
             4.downto(1).each { |idx|
-                if not @quiz["correct#{idx}"].blank?
+                if not @quiz["correct#{idx}"].nil?
                     @current_question_num = idx + 1
                     question_index = @quiz["question#{idx + 1}"]
                     break # found our question, so we're all done
                 end
             }
-            
+
             if question_index.nil?
                 #Nothing selected - must be at question 1
                 @current_question_num = 1
                 question_index = @quiz.question1
             end
         end
-        
+
         logger.debug "Quiz #{@quiz.id} Deduction: Done? #{@quiz_done}, Num:#{@current_question_num}, Q:#{@current_question}"
-        
+
         if not question_index.nil?
             @current_question = Question.find(question_index)
         end
@@ -38,24 +40,55 @@ class QuizzesController < ApplicationController
     def show
         @quiz = Quiz.find(params[:id])
         deduce_quiz_state
-        
+
         if @quiz_done
             # Quiz is completed: redirect to results
+            session[:last_answer] = ""
             redirect_to action: "results", id: params[:id]
             return
-        end        
+        end
+
+        if session[:last_answer].blank? or @current_question_num < 2
+            @last_answer = ""
+        else
+            @last_answer = session[:last_answer]
+        end
+        session[:last_answer] = ""
+
+        # Remove any unintentional dups and use a random order
+        @answers = [
+            @current_question.correct_answer,
+            @current_question.option1,
+            @current_question.option2,
+            @current_question.option3,
+            @current_question.option4
+        ].uniq.shuffle
     end
-    
+
     def answer
+        session[:last_answer] = ""
+
         @quiz = Quiz.find(params[:id])
         deduce_quiz_state
-        
-        #TODO: set correct answer
-        
+
+        correct = params[:answer] == @current_question.correct_answer
+
+        if correct
+            session[:last_answer] = "You got question #{@current_question_num} Correct!"
+            correct = true
+        else
+            session[:last_answer] = "Sorry - you got question #{@current_question_num} wrong"
+            correct = false
+        end
+
+        #Update quiz with results
+        @quiz["correct#{@current_question_num}"] = correct
+        @quiz.save()
+
         redirect_to action: "show", id: params[:id]
     end
 
-	
+
 	def create_quiz(request_category,request_level,requesting_user)
 		#num_questions=Question.where(category: category).last.id
 		#Question.where(id: [(1..num_questions].sample(5))
@@ -73,13 +106,13 @@ class QuizzesController < ApplicationController
 	#	question2: questions[2],
 	#	question3: questions[3],
 	#	question4: questions[4],
-	#	question5: questions[5]	
-	#	})	
+	#	question5: questions[5]
+	#	})
 	end
 
-	def new 
+	def new
 	 	@questions=Question.where(category_id:params[:category], level: Integer(params[:level])).take(5)
-	
+
 		#need to change Profile.find with profile in session
 		@nwquiz=Quiz.create(user: Profile.find(1),
 				 start_date: Date.current,

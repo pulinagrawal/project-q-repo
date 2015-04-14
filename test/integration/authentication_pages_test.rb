a65d852a6fb1a8eac4bb6bbb66956801685f25d9
Craig Kelly <craig.n.kelly@gmail.com>
2015-04-13 21:12:53 -0500

    post-merge db/ut fix

--- test/integration/authentication_pages_test.rb
+++ test/integration/authentication_pages_test.rb
@@ -5,17 +5,4 @@ class AuthenticationPagesTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  require 'spec_helper'
-
-describe "Authentication" do
-
-  subject { page }
-
-  describe "signin page" do
-    before { visit signin_path }
-
-    it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
-  end
-end
 end

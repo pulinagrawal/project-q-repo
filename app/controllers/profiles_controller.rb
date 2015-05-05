6e0af9cce57415055ae1b622bddc0df25f64527c
Pulin Agrawal <pulinagrawal@users.noreply.github.com>
2015-05-05 01:35:16 +0000

    added exception handling in emailing

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,7 +13,10 @@ class ProfilesController < ApplicationController
             sign_in @profile
             flash[:success] = "Welcome to the Sample App!"
             # logger.debug(Rails.application.config.action_mailer.smtp_settings)
-            UserMailer.welcome_email(@profile).deliver_later!
+            begin
+                UserMailer.welcome_email(@profile).deliver_later!
+            rescue
+            end        
             redirect_to profile_url(@profile)
         else
             redirect_to root_url
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,10 @@ class UserMailer < ApplicationMailer
         default from: 'quizzeeq@gmail.com'
         def welcome_email(profile)
                 @profile=profile
+            begin
                 mail(to: @profile.email, subject: 'Welcome to Quizzee!')
+            rescue 
+                    puts 'email was not sent'
+            end
         end
 end
